3260a341521cc8f4095df426ed4295dc9e6220f5
alexis Algoud <alexis@atm-consulting.fr>
2017-05-17 15:57:36 +0200

    add model to module

--- core/modules/modEdedoc.class.php
+++ core/modules/modEdedoc.class.php
@@ -89,7 +89,8 @@ class modEdedoc extends DolibarrModules
 		//							'workflow' => array('WORKFLOW_MODULE1_YOURACTIONTYPE_MODULE2'=>array('enabled'=>'! empty($conf->module1->enabled) && ! empty($conf->module2->enabled)', 'picto'=>'yourpicto@ededoc')) // Set here all workflow context managed by module
 		//                        );
 		$this->module_parts = array(
-				'hooks'=>array('invoicecard')
+				'models' => 1
+				,'hooks'=>array('invoicecard')
 		);
 
 		// Data directories to create when module is enabled.
